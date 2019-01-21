127892e4a89b9a39fad5e273cf953e53b2dd3c5e
Victor Cartier <cartier.victor3@gmail.com>
2019-01-21 14:03:12 +0100

    Day 1 - morning -> some cool stuff

--- test.R
+++ test.R
@@ -1,1 +1,24 @@
 # This is a test
+#test 2
+library(tidyverse)
+library(rio)
+library(dplyr)
+library(readr)
+suppressPackageStartupMessages(library(tidyverse))
+rne <- read_csv("Repertoire-national-des-elus.csv")
+
+#dplyr & tidyr mainly
+#the pipe: %>% (ctrl/cmd + shift + M)
+#the verbs: filter (and slice), arrange, select (and rename), distinct, mutate, group_by, summarise, sample_n (number) / sample_frac (fraction)
+#stringr might be a useful companion if you have to deal with characters/strings
+
+# == to say equal or %in% because it allows us to have several values
+
+rne %>%
+  filter(`Code sexe` == "F") %>%
+  mutate(`Code sexe` = recode (`Code sexe`, "F" = "Female", "M" = "Male")) %>%
+  arrange(`Date de naissance`) %>%
+ # select(-`Code profession`) %>%
+  group_by(`Libellé de la profession`) %>%
+  summarise(n = n(), age = mean(Age)) %>%
+  View()
